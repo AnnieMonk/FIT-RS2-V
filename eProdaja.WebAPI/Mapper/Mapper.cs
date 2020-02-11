46bf04d3fa89bd552ab362c722fe183980072b35
Anisa Ćirić <41583631+AnnieMonk@users.noreply.github.com>
2020-02-11 18:51:05 +0100

    Mapper
    Mislim da ovdje treba dodati createMap za sve ostale klase, jer inače visual studio 2019 vraća error.

--- eProdaja.WebAPI/Mapper/Mapper.cs
+++ eProdaja.WebAPI/Mapper/Mapper.cs
@@ -8,10 +8,16 @@ namespace eProdaja.WebAPI.Mapper
 {
     public class Mapper : Profile
     {
-        public Mapper()
+         public Mapper()
         {
             CreateMap<Database.Korisnici, Model.Korisnici>();
+            CreateMap<Database.VrsteProizvoda, Model.VrsteProizvoda>();
+            CreateMap<Database.JediniceMjere, Model.JediniceMjere>();
+            CreateMap<Database.Proizvodi, Model.Proizvodi>();
+            CreateMap<Database.Uloge, Model.Uloge>();
             CreateMap<Database.Korisnici, Model.Requests.KorisniciInsertRequest>().ReverseMap();
+            CreateMap<Database.Proizvodi, Model.Requests.ProizvodSearchRequest>().ReverseMap();
+            CreateMap<Database.Proizvodi, Model.Requests.ProizvodUpsertRequest>().ReverseMap();
         }
     }
 }
